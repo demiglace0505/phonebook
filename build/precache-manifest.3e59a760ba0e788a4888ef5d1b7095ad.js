9f69c29a7c0d6f8cb2eaa156a736d2d97d86d3d4
demiglace0505 <60604762+demiglace0505@users.noreply.github.com>
2020-06-09 10:11:27 +0800

    added procfile

---
+++ build/precache-manifest.3e59a760ba0e788a4888ef5d1b7095ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34173783f386adf0d8c7fa9e27027b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39de5c77e07cb03a0f51",
+    "url": "/static/css/main.f01f4083.chunk.css"
+  },
+  {
+    "revision": "49bdc01c5b65f8d5ccd5",
+    "url": "/static/js/2.3b9df0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b9df0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39de5c77e07cb03a0f51",
+    "url": "/static/js/main.5aa8b252.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
